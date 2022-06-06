52c98f78bbcd0f0c1997387fbc490cfdd32b05f4
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-06-06 22:01:48 +0530

    database file added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const connect = require("../config/db")
+const cors = require("cors")
+require("dotenv").config();
+const app = express()
+const port = process.env.PORT | 1234
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.listen(port, async(req,res)=>{
+    try {
+        await connect()
+        console.log(`connected to ${port}`)
+    } catch (error) {
+        console.log(error.message);
+    }
+})
