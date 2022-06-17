9b43147c7b3b21221cc6e0a5778615287027149c
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-06-17 18:12:28 +0530

    model created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const connect = require("../config/db")
 const cors = require("cors")
 require("dotenv").config();
+
 const app = express()
 const port = process.env.PORT | 1234
 
